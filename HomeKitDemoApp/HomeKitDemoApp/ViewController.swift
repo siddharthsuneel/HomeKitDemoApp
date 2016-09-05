8032450021aa79f17658b990617c8b93898ec754
Siddharth suneel <ssuneel@dminc.com>
2016-09-05 17:37:42 +0530

    initial commit with HomeKit Setup

---
+++ HomeKitDemoApp/HomeKitDemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeKitDemoApp
+//
+//  Created by Siddharth Suneel on 05/09/16.
+//  Copyright © 2016 SiddharthSuneel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
