021649a8ff0feef3f3de8f691322654557d365e8
Siddharth suneel <ssuneel@dminc.com>
2016-09-08 16:28:05 +0530

    Connect Bridge UI Implemented.

--- HomeKitDemoApp/HomeKitDemoApp/ConnectcBridgeViewController.swift
+++ HomeKitDemoApp/HomeKitDemoApp/ConnectcBridgeViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConnectcBridgeViewController: UIViewController {
 
+    @IBOutlet var bridgeLbl: UILabel!
+    @IBOutlet var connectBridgeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.initialSetup()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    // MARK: Private Methods
+    
+    func initialSetup(){
+        connectBridgeBtn.layer.borderWidth = 1.0
+        connectBridgeBtn.layer.borderColor = UIColor.blackColor().CGColor
+        
+    }
+    
+    @IBAction func connectBridgeAction(sender: AnyObject) {
+        
+    }
 
 }
 
